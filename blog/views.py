dab3ab585fd58ea63b9083eee5a197feeefc4e70
sanghyun <zxd46@likelion.org>
2019-05-22 19:24:52 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.core.paginator import Paginator
+from .models import Blog
+from .form import BlogPost
+# Create your views here.
+
+def home(request):
+    blogs = Blog.objects
+    # 블로그 모든 글들을 대상으로 
+    blog_list = Blog.objects.all()
+    # 블로그 객체 3 개를 한 페이지로 자르기
+    paginator = Paginator(blog_list, 3)
+    # request된 페이지가 뭔지 알아내고 
+    page = request.GET.get('page')
+    # request된 페이지를 얻어온 뒤 return 해준다
+    posts = paginator.get_page(page)
+
+    return render(request, 'home.html', {'blogs':blogs , 'posts':posts})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'detail.html', {'blog': blog_detail})
+
+def new(request): # new.html을 띄워주는 함수
+    return render(request, 'new.html')
+
+def create(request): # 입력한 내용을 DB에 넣는 함수
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save() # 블로그 객체에 가져온 값을 저장 
+    return redirect('/blog/' + str(blog.id)) # 완료 후에 해당 url로 이동
+
+def blogpost(request):
+    # 사용자가 입력한 내용을 처리하는 기능 -> POST
+    if request.method=='POST':
+        form = BlogPost(request.POST) # 입력된 값들을 BlogPost모델 형식으로 받아옴
+        if form.is_valid:
+            post = form.save(commit = False)
+            post.pub_date=timezone.now()
+            post.save()
+            return redirect('/')
+
+    # 빈 페이지를 띄워주는 기능 -> GET
+    else:
+        form = BlogPost()
+        return render(request, 'new.html', {'form' : form})
+    return render(request, 'new.html')
---
+++ portfolio/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length = 255)
+    # 업로드된 이미지들을 images 폴더 안에 넣어라!
+    image = models.ImageField(upload_to = 'images/')
+    description = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return self.title
